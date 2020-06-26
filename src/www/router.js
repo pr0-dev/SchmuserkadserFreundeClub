f6eae7e75a7d8747b3eebc616c4fda6740fbac0d
NullDev (Chris) <shadow@nulldev.org>
2020-06-26 20:51:01 +0200

    added router

---
+++ src/www/router.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// =========================== //
+// = Copyright (c) TheShad0w = //
+// =========================== //
+
+// Utils
+let conf = require("../utils/configHandler");
+let log = require("../utils/logger");
+
+// Services
+let getRoutes = require("../services/getRoutes");
+let robotsHandler = require("./modules/robotsHandler");
+
+let routes;
+
+module.exports = function(app){
+    app.get("/", (req, res) => {
+        res.render("pages/index", {
+            "routeTitle": "SFC",
+            "route": req.path,
+        });
+    });
+
+    // Handler
+    app.get("/robots.txt", robotsHandler);
+
+    // Errors
+    app.get("*", (req, res) => {
+        res.render("errors/404", {
+            "routeTitle": "Nicht gefunden :(",
+            "route": req.path,
+        });
+    });
+
+    routes = getRoutes(app);
+    for (let i in routes){
+        log.info(`Route ${routes[i].path} registered with methods ${(routes[i].methods).join(", ")}`);
+    }
+};
